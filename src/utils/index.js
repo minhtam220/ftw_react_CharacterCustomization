95b1e6340d39b8f70d87fb02edaaf27c7973872e
minhtam220 <minhtam220@gmail.com>
2023-03-10 09:37:24 +0700

    use switch case in returnPartURL

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,35 +1,30 @@
 export const returnPartURL = (part, index) => {
-  const bodyURL = `images/character/`;
+  const bodyURL = "images/character/";
   const clothesURL = "images/character/clothes/";
   const accessoriesURL = "images/character/accessories/";
   const extension = ".png";
 
-  if (
-    part === "body" ||
-    part === "eyes" ||
-    part === "hair" ||
-    part === "mouths" ||
-    part === "noses" ||
-    part === "eyebrows"
-  ) {
-    // eslint-disable-next-line
-    return bodyURL + part + "/" + index.toString() + extension;
-  } else if (part === "clothes1") {
-    // eslint-disable-next-line
-    return clothesURL + "layer_1" + "/" + index.toString() + extension;
-  } else if (part === "clothes2") {
-    // eslint-disable-next-line
-    return clothesURL + "layer_2" + "/" + index.toString() + extension;
-  } else if (part === "clothes3") {
-    // eslint-disable-next-line
-    return clothesURL + "layer_3" + "/" + index.toString() + extension;
-  } else if (
-    part === "earrings" ||
-    part === "glasses" ||
-    part === "hats" ||
-    part === "neckwear"
-  ) {
-    // eslint-disable-next-line
-    return accessoriesURL + part + "/" + index.toString() + extension;
+  switch (part) {
+    case "body":
+    case "eyes":
+    case "hair":
+    case "mouths":
+    case "noses":
+    case "eyebrows":
+    case "noses":
+      return bodyURL + part + "/" + index.toString() + extension;
+    case "clothes1":
+      return clothesURL + "layer_1" + "/" + index.toString() + extension;
+    case "clothes2":
+      return clothesURL + "layer_2" + "/" + index.toString() + extension;
+    case "clothes3":
+      return clothesURL + "layer_3" + "/" + index.toString() + extension;
+    case "earrings":
+    case "glasses":
+    case "hats":
+    case "neckwear":
+      return accessoriesURL + part + "/" + index.toString() + extension;
+    default:
+      break;
   }
 };
